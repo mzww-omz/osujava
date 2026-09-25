0f179b04c5822df54a9d7e3d5a1900f30574a636
mzww-omz <295543183+mzww-omz@users.noreply.github.com>
2026-09-25 14:22:30 +0900

    test: cover osu beatmap parsing

---
+++ core/src/test/java/dev/osujava/beatmap/parse/BeatmapFileParserTest.java
@@ -0,0 +1,93 @@
+package dev.osujava.beatmap.parse;
+
+import dev.osujava.beatmap.BeatmapFile;
+import dev.osujava.beatmap.HitObject;
+import dev.osujava.beatmap.TimingPoint;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BeatmapFileParserTest {
+    private final BeatmapFileParser parser = new BeatmapFileParser();
+
+    @Test
+    void parsesMetadataSettingsEventsTimingAndObjects() throws Exception {
+        BeatmapFile file = parser.parse("""
+                osu file format v14
+                [General]
+                AudioFilename: song name.ogg
+                Mode: 0
+                [Metadata]
+                Title: Sakura
+                TitleUnicode: 桜
+                Artist: Artist
+                ArtistUnicode: 作曲者
+                Creator: Mapper
+                Version: Hard
+                [Difficulty]
+                HPDrainRate: 6
+                CircleSize: 4
+                OverallDifficulty: 7
+                ApproachRate: 8
+                SliderMultiplier: 1.6
+                SliderTickRate: 2
+                [Events]
+                //Background and video events
+                0,0,"背景,夜景.png",0,0
+                [TimingPoints]
+                1000,500,4,1,0,80,1,0
+                [HitObjects]
+                64,96,1500,1,0
+                128,192,2000,2,2,300,2,0,0
+                256,192,2500,8,0,3000
+                """, "test.osu");
+
+        assertEquals(14, file.formatVersion());
+        assertEquals("桜", file.displayTitle());
+        assertEquals("作曲者", file.displayArtist());
+        assertEquals("song name.ogg", file.difficulty().audioFilename());
+        assertEquals("背景,夜景.png", file.difficulty().backgroundFilename());
+        assertEquals(0, file.difficulty().mode());
+        assertEquals(8, file.difficulty().settings().approachRate());
+        assertEquals(1.6, file.difficulty().settings().sliderMultiplier());
+        assertEquals(1, file.difficulty().timingPoints().size());
+
+        TimingPoint point = file.difficulty().timingPoints().getFirst();
+        assertEquals(1000, point.timeMs());
+        assertEquals(500, point.beatLength());
+        assertEquals(80, point.volume());
+        assertEquals(3, file.difficulty().hitObjects().size());
+        assertEquals(HitObject.Type.CIRCLE, file.difficulty().hitObjects().getFirst().type());
+        assertEquals(64, file.difficulty().hitObjects().getFirst().x());
+        assertEquals(HitObject.Type.SLIDER, file.difficulty().hitObjects().get(1).type());
+        assertEquals(HitObject.Type.SPINNER, file.difficulty().hitObjects().get(2).type());
+    }
+
+    @Test
+    void preservesUnsupportedModeAndUsesDefaultsForOptionalSections() throws Exception {
+        BeatmapFile file = parser.parse("""
+                osu file format v3
+                [General]
+                Mode: 3
+                [Metadata]
+                Title: Test
+                Artist: Local
+                Creator: Mapper
+                Version: Mania
+                [HitObjects]
+                1,2,3,1,0
+                """, "mania.osu");
+
+        assertEquals(3, file.difficulty().mode());
+        assertEquals(5, file.difficulty().settings().overallDifficulty());
+        assertNull(file.difficulty().audioPath());
+        assertEquals("Test", file.displayTitle());
+    }
+
+    @Test
+    void rejectsFilesWithoutOsuHeader() {
+        assertThrows(BeatmapParseException.class, () -> parser.parse("[General]", "bad.osu"));
+    }
+}
